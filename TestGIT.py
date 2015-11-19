aaa632b1f6e0f9c324469c02205d9340d53c1b8e
rnks <rinat@nugaev.net>
2015-11-19 13:10:29 +0300

    All commited!

--- TestGIT.py
+++ TestGIT.py
@@ -1,2 +1,23 @@
-__author__ = 'nugaevrk'
-TestCode
+
+
+import sys
+
+def Hello(name):
+
+        if name == 'Alice':
+            print('Working in Alice mode )))')
+            name = name + '???'
+            name = name + '!'
+            print ('Hello', name)
+        else:
+            print(name + "Does not exist!")
+
+
+
+def main():
+    Hello(sys.argv[1])
+
+
+
+
+main()
